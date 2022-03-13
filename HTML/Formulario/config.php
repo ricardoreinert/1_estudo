422a8d67ab15f08c074afd95846800a9ecb911f0
Ricardo Reinert <ricardoreinert@gmail.com>
2022-03-12 22:28:51 -0300

    envio pastas formulario

---
+++ HTML/Formulario/config.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbHost = 'localhost';
+$dbUsername = 'administrador';
+$dbPassword = 'C@mp1n@s';
+$dbName = 'bdTeste';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+/*if ($conexao ->connect_errno){
+
+    echo "Erro de Conexão";
+} else{
+
+    echo "Sucesso na Conexão";
+}
+
+
+?>*/
---
+++ HTML/Formulario/tabela.php
@@ -0,0 +1,65 @@
+<?php
+
+include_once('config.php');
+
+$sql = "SELECT * from tab_teste";
+
+$result = $conexao->query($sql);
+
+
+
+//print_r($result);
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Formulário Teste</title>
+</head>
+<body>
+
+<div class = "m-5"> 
+
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Idade</th>
+            <th scope="col">Cidade</th>
+            </tr>
+        </thead>
+        <tbody>
+           <?php
+                while($user_data=mysqli_fetch_assoc($result)){
+
+                    echo "<tr>";
+                    echo "<td>".$user_data['id']."</td>";
+                    echo "<td>".$user_data['nome']."</td>";
+                    echo "<td>".$user_data['idade']."</td>";
+                    echo "<td>".$user_data['cidade']."</td>";
+
+
+                }
+           ?>
+        </tbody>
+
+        </table>
+</div>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+
+</html>
