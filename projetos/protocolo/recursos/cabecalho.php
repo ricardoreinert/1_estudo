44ce6cdf1e7d06a784a3a98adbe3ba672e7e392b
Ricardo Reinert <ricardoreinert@gmail.com>
2022-04-05 06:18:07 -0300

    t

---
+++ projetos/protocolo/recursos/cabecalho.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Viewport-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap5/css/bootstrap.min.css"  >
+        <!-- Importar CSS-->
+    <link rel="stylesheet" href="recursos/custom.css">
+
+<title>Sistema de Protocolos</title>
+
+
+</head>
+<body>
---
+++ projetos/protocolo/recursos/nav.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        <img src="imagem/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        Gerenciador de Protocolos
+      </a>
+      
+    </nav>
